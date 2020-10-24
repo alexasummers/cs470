f2c07e88b0a26b8aa2f98c8cc036df56aadce90e
Alexa <alexas0202@gmail.com>
2020-10-24 01:00:41 -0500

    Updated Junction Table

--- patientdrugjunction_table.sql
+++ patientdrugjunction_table.sql
@@ -10,8 +10,8 @@ FOREIGN KEY (drugID)
 	REFERENCES medicine(DrugID)
     ON DELETE RESTRICT) ENGINE=INNODB;
     
-INSERT INTO PatientDrugJunction (patientID, drugID) VALUES
-((SELECT drugID FROM medicine WHERE DrugID=1),(SELECT patientID FROM Client WHERE ClientID=1));
+-- INSERT INTO PatientDrugJunction (patientID, drugID) VALUES
+-- ((SELECT drugID FROM medicine WHERE DrugID=1),(SELECT patientID FROM Client WHERE ClientID=1));
 
     SELECT *
     FROM PatientDrugJunction
