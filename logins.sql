81789980260eb358688e4b96afb2e6b88cfb4a36
Alexa <alexas0202@gmail.com>
2020-11-20 23:43:08 -0600

    Encrypted usernames and passwords

--- logins.sql
+++ logins.sql
@@ -1,12 +1,14 @@
--- CREATE TABLE login (
---     login varchar(255) NOT NULL,
---     password varchar(255) NOT NULL
--- )ENGINE=INNODB;
+DROP TABLE IF EXISTS login;
+
+CREATE TABLE login (
+    login varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
+)ENGINE=INNODB;
 
 INSERT INTO login (login, password) VALUES
-('admin', MD5('admin')),
-('doctor', MD5('doctor')),
-('nurse', MD5('nurse'));
+(MD5('admin'), MD5('admin')),
+(MD5('doctor'), MD5('doctor')),
+(MD5('nurse'), MD5('nurse'));
 
 SELECT*
 FROM LOGIN
