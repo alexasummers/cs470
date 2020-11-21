2bfb93be0bcca6cad41726295a802f30290e5a95
Alexa <alexas0202@gmail.com>
2020-11-20 23:42:07 -0600

    Created encrypted passwords

--- logins.sql
+++ logins.sql
@@ -1,6 +1,12 @@
-CREATE TABLE login (
-    login varchar(255) NOT NULL,
-    password varchar(255) NOT NULL
-)ENGINE=INNODB;
+-- CREATE TABLE login (
+--     login varchar(255) NOT NULL,
+--     password varchar(255) NOT NULL
+-- )ENGINE=INNODB;
 
-\
+INSERT INTO login (login, password) VALUES
+('admin', MD5('admin')),
+('doctor', MD5('doctor')),
+('nurse', MD5('nurse'));
+
+SELECT*
+FROM LOGIN
