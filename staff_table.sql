954c4245e9bb61f7750f9719e1a04007f8d76e72
Alexa <alexas0202@gmail.com>
2020-11-20 19:10:35 -0600

    Added Connection.php file
    Began connecting database to PHP

--- staff_table.sql
+++ staff_table.sql
@@ -1,5 +1,4 @@
--- Table structure for table `Staff`
---
+-- DROP TABLE IF EXISTS Staff;
 
 -- CREATE TABLE `Staff` (
 --   `Staff_ID` int(4) NOT NULL,
@@ -13,12 +12,29 @@
 -- Dumping data for table `Staff`
 --
 
--- INSERT INTO `Staff` (`Staff_ID`, `LastName`, `FirstName`, `Department_ID`, `Credentials`) VALUES
+INSERT INTO `Staff` (`Staff_ID`, `LastName`, `FirstName`, `Department_ID`, `Credentials`) VALUES
 -- (1111, 'Jones', 'Susan', 1001, 'PhD'),
 -- (2222, 'Smith', 'Thomas', 1002, 'MD'),
 -- (3333, 'Wells', 'Mindy', 1003, 'DO'),
 -- (4444, 'Wilson', 'George', 1004, 'APRN'),
--- (5555, 'Bartlett', 'Diane', 1005, 'RN');
+-- (5555, 'Bartlett', 'Diane', 1005, 'RN'),
+(6666, 'Brown', 'John', 010002, 'MD'),
+(7777, 'Garcia', 'Robert', 010003, 'MD'),
+(8888, 'Anderson', 'Mary', 010004, 'DO'),
+(9999, 'Taylor', 'Patricia', 010006, 'PA'),
+(1112, 'Moore', 'Jennifer', 010007, 'RN'),
+(2223, 'Clark', 'William', 010008, 'APRN'),
+(3334, 'White', 'David', 010009, 'PhD'),
+(4445, 'Perez', 'Sarah', 010010, 'DO'),
+(5556, 'Lewis', 'Ronald', 010012, 'RN'),
+(6667, 'Young', 'Carol', 010013, 'RN'),
+(7778, 'King', 'Melissa', 010014, 'DO'),
+(8889, 'Hill', 'Amy', 010016, 'PA'),
+(1121, 'Green', 'Stephanie', 010018, 'MD'),
+(2232, 'Adams', 'Laura', 010020, 'APRN'),
+(3343, 'Baker', 'Ryan', 010021, 'PA'),
+(4454, 'Campbell', 'Jonathan', 010022, 'DO'),
+(5565, 'Davidson', 'James', 010023, 'RN');
 
 --
 -- Indexes for dumped tables
@@ -27,31 +43,31 @@
 --
 -- Indexes for table `Staff`
 --
--- ALTER TABLE `Staff`
---   ADD PRIMARY KEY (`Staff_ID`);
+ALTER TABLE `Staff`
+  ADD PRIMARY KEY (`Staff_ID`);
 
--- UPDATE staff
--- SET Department_ID = "010005"
--- WHERE Department_ID = "1001";
+UPDATE staff
+SET Department_ID = "010005"
+WHERE Department_ID = "1001";
 
--- UPDATE staff
--- SET Department_ID = "010011"
--- WHERE Department_ID = "1002";
+UPDATE staff
+SET Department_ID = "010011"
+WHERE Department_ID = "1002";
 
--- UPDATE staff
--- SET Department_ID = "010015"
--- WHERE Department_ID = "1003";
+UPDATE staff
+SET Department_ID = "010015"
+WHERE Department_ID = "1003";
 
--- UPDATE staff
--- SET Department_ID = "010017"
--- WHERE Department_ID = "1004";
+UPDATE staff
+SET Department_ID = "010017"
+WHERE Department_ID = "1004";
 
--- UPDATE staff
--- SET Department_ID = "010019"
--- WHERE Department_ID = "010023";
+UPDATE staff
+SET Department_ID = "010019"
+WHERE Department_ID = "010023";
 
--- ALTER TABLE staff
--- ADD FOREIGN KEY (Department_ID) REFERENCES department(departmentID);
+ALTER TABLE staff
+ADD FOREIGN KEY (Department_ID) REFERENCES department(departmentID);
 
 SELECT *
 FROM staff
