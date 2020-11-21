f0050bd65afcc5bfa2b2bae5f4a8272cc9047526
Alexa <alexas0202@gmail.com>
2020-11-20 22:58:11 -0600

    Client table PHP

--- hospital_php/add_patient.php
+++ hospital_php/add_patient.php
@@ -1,22 +1,14 @@
 <?php
 require_once 'connection.php';//include the connection file
 
-$Last_name=isset($_POST['Last_name'])?$_POST['Last_name']:"";
-$First_name=isset($_POST['First_name'])?$_POST['First_name']:"";
-$HouseNum=isset($_POST['HouseNum'])?$_POST['HouseNum']:"";
-$Street=isset($_POST['Street'])?$_POST['Street']:"";
-$Apartment=isset($_POST['Apartment'])?$_POST['Apartment']:"";
-$Zip=isset($_POST['Zip'])?$_POST['Zip']:"";
-$Phone=isset($_POST['Phone'])?$_POST['Phone']:"";
-$ICE_Fname=isset($_POST['ICE_Fname'])?$_POST['ICE_Fname']:"";
-$ICE_Lname=isset($_POST['ICE_Lname'])?$_POST['ICE_Lname']:"";
-$ICE_relation=isset($_POST['ICE_relation'])?$_POST['ICE_relation']:"";
-$ICE_number=isset($_POST['ICE_number'])?$_POST['ICE_number']:"";
-$patient_InformationID=isset($_POST['patient_InformationID'])?$_POST['patient_InformationID']:"";
+$ClientID=isset($_POST['ClientID'])?$_POST['ClientID']:"";
+$StaffID=isset($_POST['StaffID'])?$_POST['StaffID']:"";
+$DrugID=isset($_POST['DrugID'])?$_POST['DrugID']:"";
+$Length_of_stay=isset($_POST['Length_of_stay'])?$_POST['Length_of_stay']:"";
+$Vitals=isset($_POST['Vitals'])?$_POST['Vitals']:"";
 
-
-$SQL = "INSERT INTO patientinformation(Last_name,First_name,HouseNum,Street,Apartment,Zip,Phone,ICE_Fname,ICE_Lname,ICE_relation,ICE_number,patient_InformationID) VALUES(";
-$SQL.="'".$Last_name."', '".$First_name."', '".$HouseNum."', '".$Street."', '".$Apartment."', '".$Zip."', '".$Phone."', '".$ICE_Fname."', '".$ICE_Lname."', '".$ICE_relation."', '".$ICE_number."', '".$patient_InformationID."' )";
+$SQL = "INSERT INTO Client(StaffID,DrugID,Length_of_stay,Vitals) VALUES(";
+$SQL.="'".$StaffID."', '".$DrugID."', '".$Length_of_stay."', '".$Vitals."')";
 $result = mysqli_query($connection,$SQL);
 
 if (!$result) //if the query fails
