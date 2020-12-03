5e78e210b149e0cacc07f7bc2bf23737598b84fb
Alexa Summers <alexas0202@gmail.com>
2020-12-02 23:35:20 -0600

    Updated Client to staff junction
    Client to staff junction table updated with constraints and not nulls

--- Client_staff_junction.sql
+++ Client_staff_junction.sql
@@ -1,11 +1,16 @@
+DROP TABLE IF EXISTS clientstaffjunction;
+
+
 CREATE TABLE ClientStaffJunction
-(junctionID int AUTO_INCREMENT,
-ClientID int,
-StaffID int,
-PRIMARY KEY(junctionID),
-FOREIGN KEY(ClientID)
-REFERENCES client(ClientID)
-ON DELETE RESTRICT,
+(ClientID int NOT NULL,
+StaffID int NOT NULL,
+CONSTRAINT client_staff_pk PRIMARY KEY (ClientID, StaffID),
+CONSTRAINT client_fk
+	FOREIGN KEY (clientID) REFERENCES client(ClientID) ON DELETE RESTRICT,
+    CONSTRAINT staff_fk
 FOREIGN KEY(StaffID)
 REFERENCES staff(Staff_ID)
 ON DELETE RESTRICT) ENGINE=INNODB;
+
+SELECT *
+FROM ClientStaffJunction;
