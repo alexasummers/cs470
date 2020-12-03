e8411f670bb0e91b3bcbf7691bc565e1cac312df
Alexa Summers <alexas0202@gmail.com>
2020-12-03 00:25:17 -0600

    Updated logins
    Updated spelling mistake

--- logins.sql
+++ logins.sql
@@ -11,10 +11,11 @@ INSERT INTO login (login, password) VALUES
 (('admin'), ('$2y$10$4IL1ixYfu/Ok7X3V843tAeKInfFI90HdALiWBnBxs.mhc8RK3N8X.')),
 (('doctor'), ('$2y$10$DQwDPL8WJ8UqbueeO8.WBOGnr7p/oWPZDvvwp1KxzKJ8nI626WBdK')),
 (('nurse'), ('$2y$10$PXugIto6/qEgtkx37Fnj4.BwOC6Tx.cOksHZP83BcYdLBpTe9yYYi')),
-(('practicioner'), ('$2y$10$cLoMP.VKAmm8eogKAYYOeurNWK9uRnsIF971e9wD6Kzmay13FI46q')),
+(('practitioner'), ('$2y$10$E3LsWPOJ5MmbvqXfYeapuO9U0ZMZJhMmAeCE/cMS7cKNnA/ziqg3u')),
 (('examiner'), ('$2y$10$pyK07srlBtx07vjG8hh.C.euOjPEf07Pn2fjObkqvDHXVtip/FJ5q')),
 (('surgeon'), ('$2y$10$LjQ.RpV4hv9D094KC86YZOFYgT6DF2JgQcd/uOA1Q/1OdXEWx229S')),
 (('assistant'), ('$2y$10$xMsaQDNsjWLROPqyjbijPOKlFwRHHdevbhfePdBHHV/A9UJUHFIyu'));
+
 
 SELECT *
 FROM login;
