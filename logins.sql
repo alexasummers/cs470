fe3bc687943ce7aeb9364b65e22bc4a7c7369274
Alexa Summers <alexas0202@gmail.com>
2020-12-03 00:11:54 -0600

    Added login
    Added PA login

--- logins.sql
+++ logins.sql
@@ -13,7 +13,8 @@ INSERT INTO login (login, password) VALUES
 (('nurse'), ('$2y$10$PXugIto6/qEgtkx37Fnj4.BwOC6Tx.cOksHZP83BcYdLBpTe9yYYi')),
 (('practicioner'), ('$2y$10$cLoMP.VKAmm8eogKAYYOeurNWK9uRnsIF971e9wD6Kzmay13FI46q')),
 (('examiner'), ('$2y$10$pyK07srlBtx07vjG8hh.C.euOjPEf07Pn2fjObkqvDHXVtip/FJ5q')),
-(('surgeon'), ('$2y$10$LjQ.RpV4hv9D094KC86YZOFYgT6DF2JgQcd/uOA1Q/1OdXEWx229S'));
+(('surgeon'), ('$2y$10$LjQ.RpV4hv9D094KC86YZOFYgT6DF2JgQcd/uOA1Q/1OdXEWx229S')),
+(('assistant'), ('$2y$10$xMsaQDNsjWLROPqyjbijPOKlFwRHHdevbhfePdBHHV/A9UJUHFIyu'));
 
 SELECT *
 FROM login;
