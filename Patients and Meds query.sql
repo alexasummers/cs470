da26585b0011aefbd155e09c8a997543a5c85740
karinicole34 <74069367+karinicole34@users.noreply.github.com>
2020-12-04 14:10:33 -0600

    Update Patients and Meds query.sql

--- Patients and Meds query.sql
+++ Patients and Meds query.sql
@@ -1,10 +1,7 @@
-
-SELECT ClientID, drugID, Name
-from Client
-JOIN medicine
-on [Client].DrugID = drug.ID
-as TEMP;
-SELECT ClientID, Last_Name, First_Name, Name, drugID
-FROM Patientinformation
-LEFT JOIN TEMP
-on ClientID = ClientID;
+SELECT t.ClientID, p.Last_Name, p.First_Name, t.Name, t.drugID
+FROM Patientinformation p
+LEFT JOIN (SELECT c.ClientID, c.drugID, m.Name
+from Client c
+inner JOIN medicine m
+on c.DrugID = m.drugID) as t
+on t.ClientID = p.patient_informationID;
