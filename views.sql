3997915f48eafbe58dcac1dd2705c09cde25e3b4
Alexa Summers <alexas0202@gmail.com>
2020-12-04 15:27:05 -0600

    Fixed bug in view
    Fixed bug in view on Client -> patient

--- views.sql
+++ views.sql
@@ -9,4 +9,4 @@ FROM PatientInformation,client;
 CREATE VIEW Nurse AS
 SELECT PatientInformation.Last_name,PatientInformation.First_name,Client.ClientID,Client.DrugID,Client.Length_of_stay,Client.Vitals
 FROM PatientInformation
-INNER JOIN Client ON PatientInformation.patient_InformationID=Client.ClientID;
+INNER JOIN Client ON PatientInformation.client_InformationID=Client.ClientID;
