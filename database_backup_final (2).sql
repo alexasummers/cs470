3ee284b6db64c851e10962e5af06b7a308ec5aa6
Alexa Summers <alexas0202@gmail.com>
2020-12-06 10:32:46 -0600

    Update database backup

--- database_backup_final (2).sql
+++ database_backup_final (2).sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `test`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: test
@@ -422,4 +424,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-06 10:27:41
+-- Dump completed on 2020-12-06 10:32:00
